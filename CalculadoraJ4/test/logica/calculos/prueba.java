cdbf8f2e5add6d2b28c6972360056866832ce2f1
EndOfBehelit <jcarba10@hotmail.com>
2025-10-15 12:57:09 +0200

    Pruebas git.

---
+++ CalculadoraJ4/test/logica/calculos/prueba.java
@@ -0,0 +1,14 @@
+package logica.calculos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class prueba {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
